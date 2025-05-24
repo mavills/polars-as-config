b72d087b47bbbc7b3ccbe566717a2124c29ddb37
Stijn De Pauw <stijn-depauw@hotmail.be>
2025-05-24 16:03:11 +0200

    Create config parser

--- tests/test_basic.py
+++
@@ -1,9 +0,0 @@
-"""Basic tests for the package."""
-
-from polars_as_config import __version__
-
-
-def test_version():
-    """Test that version is a string."""
-    assert isinstance(__version__, str)
-    assert len(__version__) > 0 
